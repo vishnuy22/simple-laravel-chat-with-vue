d02737ee444915f4d980977477e2a80549fb6242
vishnuy22 <46164293+vishnuy22@users.noreply.github.com>
2022-03-31 12:54:40 +0530

    First set of data via upload

---
+++ storage/framework/views/9edadb340f7527b756cfb9b56c015f430ee90511.php
@@ -0,0 +1,18 @@
+<!-- resources/views/chat.blade.php -->
+
+
+
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="card">
+        <div class="card-header">Chats</div>
+        <div class="card-body">
+            <chat-messages :messages="messages"></chat-messages>
+        </div>
+        <div class="card-footer">
+            <chat-form v-on:messagesent="addMessage" :user="<?php echo e(Auth::user()); ?>"></chat-form>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/laravel-chat1/resources/views/chat.blade.php ENDPATH**/ ?>
